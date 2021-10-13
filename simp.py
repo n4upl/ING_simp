2f073e2e73e6be687b136ab3281af612c68c9377
Piotr Wilkosz <piotr.wilkosz.ext@deutsche-boerse.com>
2021-10-13 21:30:51 +0200

    implement option to get more than one report per day.

--- simp.py
+++ simp.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 import logging
 logging.basicConfig(level=logging.INFO)
 import requests
@@ -9,6 +10,7 @@ import textwrap
 import datetime
 import argparse
 import configparser
+import base64
 #import lxml
 from lxml import etree as ET
 from lxml import objectify
@@ -79,6 +81,7 @@ def send_mail(send_from, send_to, subject, message, files=[], server="localhost"
     smtp.sendmail(send_from, send_to, msg.as_string())
     smtp.quit()
 
+
 class SimpReportException(Exception):
     pass
 
@@ -90,8 +93,9 @@ class SimpReport:
         self.args = args
         self.simp_report_date = args.date
         self.simp_code = args.simpcode
+        self.reports = []
 
-    def get_soap_request_body(self, msg_id, rpt_id, date):
+    def get_soap_request_body(self, msg_id, rpt_id, date, rpt_format='XML'):
         soap_request_body_template = """
             <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ca:std:ccs:ing:tech:xsd:mhdr.001.001.01" xmlns:urn1="urn:ca:std:cdc:tech:xsd:ing.cdc.001.01" xmlns:urn2="urn:ca:std:ccs:ing:tech:xsd:rpts.013.001.01">
                <soapenv:Header/>
@@ -117,6 +121,7 @@ class SimpReport:
                                        <urn2:RptId>
                                           <urn2:EQ>{rpt_id}</urn2:EQ>
                                        </urn2:RptId>
+                                       <urn2:RptFrmt>{rpt_format}</urn2:RptFrmt>
                                     </urn2:SchCrit>
                                  </urn2:NewCrit>
                               </urn2:RptMCCrit>
@@ -127,13 +132,14 @@ class SimpReport:
                </soapenv:Body>
             </soapenv:Envelope>
         """
-    
-        return textwrap.dedent(soap_request_body_template).format(msg_id=msg_id, date=date, rpt_id=rpt_id, simp_code=self.simp_code)
+        return textwrap.dedent(soap_request_body_template).format(msg_id=msg_id, date=date, rpt_id=rpt_id, simp_code=self.simp_code, rpt_format=rpt_format)
 
-    def send_soap_request(self, msg_id, rpt_id, date=None):
+
+    def send_soap_request(self, msg_id, rpt_id, date=None, rpt_format='XML'):
+        status = True
         if date is None:
             date = self.simp_report_date
-        soap_request = self.get_soap_request_body(msg_id, rpt_id, date)
+        soap_request = self.get_soap_request_body(msg_id, rpt_id, date, rpt_format)
         r = requests.post(url=wsdl_url, cert=cert_file, data=soap_request, headers=headers)
         soap_response = remove_namespace(ET.ElementTree(ET.fromstring(r.text)))
         if self.args.verbose:
@@ -142,63 +148,125 @@ class SimpReport:
 
         err = soap_response.xpath('//RuleDesc/text()')
         if len(err):
-            logging.error(f"Error processing soap request: {err}")
+            if len(self.reports) == 0:  # following error should be printed in case of processing first item only.
+                logging.error(f"Error processing soap request. date: {date} rpt_id: {rpt_id} ERR: {err}")
             logging.debug(f"SOAP request: {soap_request}")
             logging.debug(f"SOAP response: {ET.tostring(soap_response)}")
+            status = False
             #if not self.args.force:
-            #    #raise SimpReportException(f"{err}")
-            #    logging.error(f"There are no simp reports for: {self.simp_report_date}. {err}")
+            #    logging.exception(f"There are no simp reports for: {self.simp_report_date}. {err}")
+            #    raise SimpReportException(f"{err}")
 
-        return soap_response
+        return soap_response, status
+
 
     def get_rpt_id(self, date=None, rpt_id=0):
         if date is None:
             date = self.simp_report_date
         msg_id = datetime.datetime.now().strftime('%s')
-        xml_response = self.send_soap_request(msg_id, rpt_id, date)
-        rpt_id = xml_response.xpath('//GetMCReportResponse/Document/MCRpt/Rpt/RptId/EQ/text()')[0]
+        xml_response, status = self.send_soap_request(msg_id, rpt_id, date, rpt_format='XML')
+        if status is not False:
+            rpt_id = xml_response.xpath('//GetMCReportResponse/Document/MCRpt/Rpt/RptId/EQ/text()')[0]
 
-        return int(rpt_id)
+        return int(rpt_id), status
 
-    def get_single_report(self, date=None, rpt_id=0):
+
+    def get_reports(self, date=None, raw=True):
+        """
+            Gets all SIMP reports for a given day.
+        """
+
+        self.reports = []  # reset reports table
+
+        report = self.get_report(date, 0, raw)
+        while report['status'] is True:
+            next_report_id = report['rpt_id'] + 1
+            report = self.get_report(date, next_report_id, raw)
+
+        return report
+
+
+    def get_report(self, date=None, rpt_id=0, raw=True):
         if date is None:
             date = self.simp_report_date
 
         data = {
             'simp_report': None,
             'date': date,
-            'n_records': 0,
+            'trans_count': 0,
             'rpt_id': 0,
+            'status': True,
         }
-        data['rpt_id'] = self.get_rpt_id(data['date'], rpt_id) + 1
+        data['rpt_id'], data['status'] = self.get_rpt_id(data['date'], rpt_id)
         msg_id = datetime.datetime.now().strftime('%s')
-        xml_response = self.send_soap_request(msg_id, data['rpt_id'], data['date'])
-        ntrys = xml_response.xpath('//Ntry')
-        logging.debug("number of Ntry records:", len(ntrys))
-        data['n_records'] = len(ntrys)
-        if len(ntrys) > 0:
-            data['simp_report'] = self.ntrys2simp(ntrys)
-        else:
-            logging.info(f"There are no simp reports for date: {data['date']} rpt_id: {data['rpt_id']}")
+
+        if raw is True:
+            xml_data, status = self.send_soap_request(msg_id, data['rpt_id'], data['date'], rpt_format='RAW')
+            data['simp_report'], data['simp_report_fn'] = self.process_report_file_xml(xml_data)
+            trn = re.findall('"il.trn.:(\d+) ', data['simp_report'])
+            if len(trn) > 0:
+                data['trans_count'] = int(trn[0])
+            self.reports.append(data)
+
+        else:  # use XML format for SOAP responses
+            xml_response, status = self.send_soap_request(msg_id, data['rpt_id'], data['date'])
+            ntrys = xml_response.xpath('//Ntry')
+            logging.debug("number of Ntry records:", len(ntrys))
+            data['trans_count'] = len(ntrys)
+            data['simp_report_fn'] = '{prefix}_{date}_{iter}.txt'.format(prefix="ING-SIMP", date=self.args.date, iter=data['rpt_id'])
+            if len(ntrys) > 0:
+                data['simp_report'] = self.ntrys2simp(ntrys)
+            else:
+                data['simp_report'] = ''
+                #logging.info(f"There are no simp reports for date: {data['date']} rpt_id: {data['rpt_id']}")
+            self.reports.append(data)
 
         return data
-    
-    def get_reports(self, date=None):
+
+
+    def process_report_file_xml(self, xml_data):
         """
-            Gets all SIMP reports for a given day.
-            TODO: implement option to get more than one report per day.
+        <Envelope>
+          <Body>
+            <GetMCReportResponse>
+              <Document>
+                <Rpt>
+                  <MsgId>
+                    <Id>ING_CCS_MC_20211013_173050769</Id>
+                  </MsgId>
+                  <RptDtls>
+                    <RptSts>R</RptSts>
+                    <RptNm>netforyou_202109172</RptNm>
+                    <RptCreDt>2021-09-17</RptCreDt>
+                    <RptType>SIMP_FILE</RptType>
+                    <RptSize>312</RptSize>
+                    <RptFile>PFNJTVAyPjEwNTAwMTYxNzU2NDAwMDAwMDAwMDAwMDAwLDIwMjEtMDktMTcNCjY4MTA1MDAxNjE3NTY0MDAwMDAwMDAwMDA0LDg3MDAsQyxQTE4sMjAyMS0wOS0xNyxVWk4sOTcyMDE4NzAwMjc4LCwiNzgxMTYwMjIwMjAwMDAwMDA0NjI2NDA3MDYiLCJMQUJFVFNLSSBTVEFOSVNMQVYiLCIiLCJXUtNCTEVXSUNFIFVMIFNURUZBTkEgQkFUT1JFR08gN003IiwiNTUtMzMwICAgIE1JyktJTklBIiwiUHJ6ZWxldyBrcmFqb3d5IC0gTkVDSU9SIiwiIiwiIiwiIixFLCwyMDIxLTA5LTE2DQo8L1NJTVAyPiJpbC50cm4uOjEgd2FydC50cm4uOjg3LjAwIg0K</RptFile>
+                    <FileChecksum>A5FA5C2F7EFFFE9ABD7FBD930E847C06</FileChecksum>
+                  </RptDtls>
+                </Rpt>
+              </Document>
+            </GetMCReportResponse>
+          </Body>
+        </Envelope>
         """
 
-        report = self.get_single_report(date, 0)
-        #report2 = self.get_single_report(date, report['rpt_id'])
-        
-        return report
-        
+        rpt = xml_data.xpath('//RptDtls')
+        try:
+            encoded_data = rpt[0].xpath('./RptFile')[0].text
+            simp_report = base64.b64decode(encoded_data).decode(self.args.bank_encoding)
+        except IndexError:
+            simp_report = ''
+        try:
+            simp_report_fn = rpt[0].xpath('./RptNm')[0].text + '.txt'
+        except IndexError:
+            simp_report_fn = 'temp_file_{date}.txt'.format(date=self.args.date)
+
+        return simp_report, simp_report_fn
+
 
     def ntrys2simp(self, ntrys=[]):
         """
             converts series of <Ntry/> xml elements to simp raport format
-
         """
 
         data = {}
@@ -251,40 +319,48 @@ class SimpReport:
         
         return ret
 
-    def get(self, mode):
-        report = self.get_reports(self.args.date)
 
-        if report['simp_report'] is not None:
-            print(report['simp_report'])
+    def get(self, mode):
+        self.get_reports(self.args.date)
+
+        for report in self.reports:
+            if report['status'] is True and report['trans_count'] > 0:
+                print(report['simp_report'])
 
         return True
+
 
     def save(self, mode):
-        report = self.get_reports(self.args.date)
-        fn = '{prefix}_{date}_{iter}.txt'.format(prefix="ING-SIMP", date=self.args.date, iter=1)
-        if report['simp_report'] is not None:
-            with open(fn, 'w') as f:
-                f.write(report['simp_report'])
-            logging.info(f"simp_report saved: {fn}")
+        self.get_reports(self.args.date)
+
+        for report in self.reports:
+            if report['status'] is True and report['trans_count'] > 0:
+                fn = report['simp_report_fn']
+                with open(fn, 'w') as f:
+                    f.write(report['simp_report'])
+                logging.info(f"simp_report saved: {fn}")
 
         return True
 
+
     def send(self, mode):
-        report = self.get_reports(self.args.date)
-        fn = '{prefix}_{date}_{iter}.txt'.format(prefix="ING-SIMP", date=self.args.date, iter=1)
-        if report['simp_report'] is not None:
-            with open(fn, 'w') as f:
-                f.write(report['simp_report'])
+        self.get_reports(self.args.date)
 
-            logging.info(f"simp_report saved: {fn}")
-            subject = f"ING SIMP report at {self.args.date}"
-            message = f'Dear Colleague, please find attached SIMP report generated for: {self.args.date}'
-            files = [fn]
-            send_to = [self.args.mail_to, self.args.mail_from]
+        subject = f"ING SIMP report at {self.args.date}"
+        message = f'Dear Colleague, please find attached SIMP report generated for: {self.args.date}'
+        send_to = [self.args.mail_to, self.args.mail_from]
+        files = []
+        for report in self.reports:
+            if report['status'] is True and report['trans_count'] > 0:
+                fn = report['simp_report_fn']
+                with open(fn, 'w') as f:
+                    f.write(report['simp_report'])
+                logging.info(f"simp_report saved: {fn}")
+                files.append(fn)
+
+        if len(files) > 0:
             send_mail(self.args.mail_from, send_to, subject, message, files, self.args.mail_host, self.args.mail_port, self.args.mail_user, self.args.mail_pass, use_tls=True, use_auth=True)
-            
             logging.info(f"simp_report sent via email to: {send_to}")
-
 
         return True
 
@@ -322,7 +398,7 @@ def main():
     parser.add_argument("--mail_port", help="mail server port number. (default: %(default)s)")
     parser.add_argument("--mail_user", help="mail server user. (default: %(default)s)")
     parser.add_argument("--mail_pass", help="mail server password. (default: %(default)s)")
-
+    parser.add_argument("--bank_encoding", default='ISO-8859-2', help="encoding used by the bank. (default: %(default)s)")
 
     args = parser.parse_args(remaining_argv)
 
